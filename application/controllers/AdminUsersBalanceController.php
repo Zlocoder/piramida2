d552bf54f6e9a67ab35fe4ad4ffbe5445299f8a0
George Lemish <yorgus@yandex.ru>
2017-05-31 09:57:28 +0300

    user payment recalc

--- application/controllers/AdminUsersBalanceController.php
+++ application/controllers/AdminUsersBalanceController.php
@@ -19,6 +19,17 @@ class AdminUsersBalanceController extends \app\base\AdminController {
         ]);
     }
 
+    public function actionCalculate() {
+        $users = User::find()->with(['status', 'payment', 'invoices.transactions'])->joinWith('status', true, 'INNER JOIN');
+
+        $batch = [];
+        foreach ($users as $user) {
+            foreach ($user->invoices as $invoice) {
+                if ($invoice->invoiceStatus == 'payed' || 'complete');
+            }
+        }
+    }
+
     public function actionView($id) {
         return $this->render('one', [
             'user' => User::findOne($id)
